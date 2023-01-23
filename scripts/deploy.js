fcbfe7c9e423564b60c43ab6801bf60ef001bfab
Abhinav Thapa <abhinavthapa1998@gmail.com>
2023-01-23 22:38:48 +0530

    Deploy Script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,21 +6,47 @@
 // global scope, and execute the script.
 const hre = require("hardhat");
 
+async function getBalances(address) {
+  const balanceBigInt = await hre.ethers.provider.getBalance(address);
+  return hre.ethers.utils.formatEther(balanceBigInt);
+}
+async function consoleBalances(addresses) {
+  let counter = 0;
+  for (const address of addresses) {
+    console.log(`Address ${counter} balance:`, await getBalances(address));
+  }
+}
+async function consoleMemos(memos) {
+  for (const memo of memos) {
+    const timestamp = memo.timestamp;
+    const name = memo.name;
+    const from = memo.address;
+    const message = memo.message;
+    console.log(
+      `At ${timestamp}, name ${name}, address ${from}, message ${message}`
+    );
+  }
+}
+
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const [owner, from1, from2, from3] = await hre.ethers.getSigners();
+  const coffee = await hre.ethers.getContractFactory("coffee");
+  const contract = await coffee.deploy();
+  await contract.deployed();
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  console.log("Address of Contract:", contract.address);
+  const addresses = [owner.address, from1.address];
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  console.log("Before Buying Coffee");
+  await consoleBalances(addresses);
 
-  await lock.deployed();
+  const amount = { value: hre.ethers.utils.parseEther("1") };
+  await contract.connect(from1).buyCoffee("from1", "Great Coffee", amount);
+  await contract.connect(from2).buyCoffee("from2", "Loved it!", amount);
+  await contract.connect(from3).buyCoffee("from3", "Will return!", amount);
 
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log("After Buying Coffee");
+  await consoleBalances(addresses);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
